de6adbb932a44deb36bc6226646b2a58bdc43588
nicolas6042 <nicolas6042@gmail.com>
2018-02-22 21:49:59 +0100

    First Tests

--- spring-testing/src/main/java/com/domain/testing/TestingMain.java
+++ spring-testing/src/main/java/com/domain/testing/TestingMain.java
@@ -1,7 +1,40 @@
 package com.domain.testing;
 
+import com.domain.testing.User.SimpleUser;
+import com.domain.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String []args){
-        System.out.println("Wprowadzenie do testowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if(result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("ERROR!");
+        }
+
+        System.out.println("Test jednostkowy");
+
+        Calculator calculator = new Calculator();
+
+        int addCheck = 4;
+        int substractCheck = 1;
+
+       int addResult = calculator.add(2,2);
+        int substractResult = calculator.substract(2,1);
+
+        if(addResult == addCheck){
+            System.out.println("test add ok");
+        } else  {
+            System.out.println("Error");
+        }
+
+        if(substractResult == substractCheck){
+            System.out.println("test subst ok");
+        } else{
+            System.out.println("Error");
+        }
     }
 }
---
+++ spring-testing/src/main/java/com/domain/testing/User/SimpleUser.java
@@ -0,0 +1,13 @@
+package com.domain.testing.User;
+
+public class SimpleUser {
+    private String username;
+
+    public SimpleUser(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
