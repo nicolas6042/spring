b5b72a0fa7fdf0986c4372b2b1859ca16015a073
Mikolaj Baranowicz <m.baranowicz@fireup.pro>
2018-03-19 07:47:08 +0100

    Commit

---
+++ spring-hibernate/src/main/java/com/domain/springhibernate/SpringHibernateApplication.java
@@ -0,0 +1,12 @@
+package com.domain.springhibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHibernateApplication.class, args);
+	}
+}
--- spring-jdbc/src/test/java/com.domain.jdbc/DbManagerTestSuite.java
+++ spring-jdbc/src/test/java/com.domain.jdbc/DbManagerTestSuite.java
@@ -2,9 +2,12 @@ package com.domain.jdbc;
 
 import org.junit.Test;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import static junit.framework.TestCase.assertNotNull;
+import static org.junit.Assert.assertEquals;
 
 /**
  * @author mbaranowicz
@@ -18,5 +21,53 @@ public class DbManagerTestSuite {
         DbManager dbManager = DbManager.getInstance();
         //Then
         assertNotNull(dbManager.getConnection());
+    }
+
+    @Test
+    public void testSelectUsers() throws SQLException{
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+
+        //When
+        String sqlQuery = "SELECT * FROM USERS";
+        Statement statement = dbManager.getConnection().createStatement();
+        ResultSet rs = statement.executeQuery(sqlQuery);
+
+        //Then
+        int counter = 0;
+        while (rs.next()){
+            System.out.println(rs.getInt("ID") + ", " +
+            rs.getString("FIRSTNAME") + ", " +
+            rs.getString("LASTNAME"));
+            counter++;
+        }
+        rs.close();
+        statement.close();
+
+        assertEquals(5,counter);
+    }
+
+    @Test
+    public void testSelectUsersAndPost() throws SQLException{
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+        //When
+        String sqlQuery = "select firstname,lastname from Users join Posts on users.id = posts.USER_ID group by firstname,lastname having count(posts.id)>=2;";
+        Statement statement = dbManager.getConnection().createStatement();
+        ResultSet resultSet = statement.executeQuery(sqlQuery);
+
+        //Then
+        int counter = 0;
+
+        while(resultSet.next()){
+            System.out.println(resultSet.getString("FIRSTNAME") + " " +
+            resultSet.getString("LASTNAME"));
+            counter++;
+        }
+        resultSet.close();
+        statement.close();
+
+        assertEquals(2,counter);
+
     }
 }
