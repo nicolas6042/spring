1f5af678ecb9640450875dcb2abffe54a3d63459
Mikolaj Baranowicz <m.baranowicz@fireup.pro>
2018-04-19 10:33:24 +0200

    update

---
+++ spring-patterns2/src/main/java/com/domain/patterns2/SpringPatterns2Application.java
@@ -0,0 +1,12 @@
+package com.domain.patterns2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPatterns2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPatterns2Application.class, args);
+    }
+}
